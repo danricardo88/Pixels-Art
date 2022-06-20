7e911aff2b62a120f3354d209ddb270874d13bd6
danricardo88 <daniel.ricardo88@outlook.com>
2022-06-20 12:58:24 -0300

    refatorado :rocket:

--- script.js
+++ script.js
@@ -1,46 +1,123 @@
-// Criando a troca de selected nas paletas de cores
-const selectedPalette = document.getElementById('color-palette').children;
+// Variáveis e constantes globais:
+const colorPaletteId = document.getElementById('color-palette');
+const pixelBoardId = document.getElementById('pixel-board');
+const selectedMove = document.getElementById('color-palette').children;
+const inputValor = document.getElementById('board-size');
+const buttonInput = document.getElementById('generate-board');
 
-function selectedAllColor(event) {
-  const selectedRandom = document.querySelector('.selected');
-  selectedRandom.classList.remove('selected');
-  var test = event.target.getAttribute('id');
-  event.target.setAttribute('cor', `${test}-cor`);
-  
-  event.target.classList.add('selected');
-}
+// criando as class 'color e selected' as divs e dando cor.
+const gerandoCor = () => {
+  return (
+    '#' +
+    parseInt(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, '0')
+  );
+};
 
-for (let index = 0; index < selectedPalette.length; index += 1) {
-  selectedPalette[index].addEventListener('click', selectedAllColor);
-}
+const divDefine = (classe, color) => {
+  const criandoDivs = document.createElement('div');
+  criandoDivs.className = classe;
+  criandoDivs.style.backgroundColor = color;
+  return criandoDivs;
+};
 
-//criando as div's para o bloco a ser colorido com a paleta
-let pixelsBloco = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25,
-];
+const addPaiDaDiv = (pai, filho) => {
+  pai.appendChild(filho);
+};
 
-function addPixels() {
-  const itemPixel = document.querySelector('#pixel-board');
-  let numerosDeElementos = pixelsBloco;
-
-  for (let index = 0; index < numerosDeElementos.length; index += 1) {
-    if (numerosDeElementos.length > index) {
-      let pixelsCreate = document.createElement('div');
-      pixelsCreate.className = 'pixel';
-      itemPixel.appendChild(pixelsCreate);
+const divsColorCreator = (number) => {
+  for (let i = 0; i < number; i += 1) {
+    if (i === 0) {
+      addPaiDaDiv(colorPaletteId, divDefine('color selected', 'black'));
+    } else {
+      addPaiDaDiv(colorPaletteId, divDefine('color', gerandoCor()));
     }
   }
+};
+divsColorCreator(4);
+
+// criando bloco de pixels brancos.
+
+const divsPixelsCreator = (numero = 5) => {
+  const qtdPixels = numero * numero;
+  pixelBoardId.style.gridTemplateColumns = `repeat(${numero}, 1fr)`;
+
+  for (let i = 0; i < qtdPixels; i += 1) {
+    addPaiDaDiv(pixelBoardId, divDefine('pixel', 'white'));
+  }
+};
+divsPixelsCreator();
+
+// criando função que movimenta o selected em uma color por vez.
+const moveSelected = (event) => {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+};
+for (let i = 0; i < selectedMove.length; i += 1) {
+  selectedMove[i].addEventListener('click', moveSelected);
 }
-addPixels();
 
-//Criando a função de selecionar cor e pintar os pixels
+// pintando os pixels branco - ESSA FOI TENSA.
+const colorir = (paint) => {
+  const color = document.querySelectorAll('.color');
+  for (let i = 0; i < color.length; i += 1) {
+    if (color[i].classList.contains('selected')) {
+      paint.target.style.backgroundColor = color[i].style.backgroundColor;
+    }
+  }
+};
+pixelBoardId.addEventListener('click', colorir);
 
-document.querySelectorAll('.pixel').forEach((item) => {
-  item.addEventListener('click', function (e) {
-    let pallet = document.querySelector('.selected');
-    let cor = pallet.getAttribute('cor');
-    this.setAttribute('class', `pixel ${cor}`);
+// criando botão de limpar
+const clearBtn = (limpar) => {
+  const buttonClass = document.querySelector('.button-container');
+  const clearButton = document.createElement('button');
+  const buttonId = 'clear-board';
+
+  clearButton.innerHTML = limpar;
+  clearButton.id = buttonId;
+  buttonClass.appendChild(clearButton);
+};
+clearBtn('Limpar');
+const btn = document.querySelector('#clear-board');
+
+const clear = () => {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
+  }
+};
+
+btn.addEventListener('click', clear);
+
+//chegando as exigencias para add pixel
+const pixelsChecagem = () => {
+  if (inputValor.value === '') {
+    alert('Board inválido!');
+    inputValor.value = 25;
+  } else if (inputValor.value < 5) {
+    inputValor.value = 5;
+  } else if (inputValor.value > 50) {
+    inputValor.value = 50;
+  }
+  return inputValor.value;
+};
+
+// chamando e add os pixels
+const pixelsAdd = () => {
+  buttonInput.addEventListener('click', () => {
+    delDivsPixel();
+    numero = pixelsChecagem();
+    divsPixelsCreator(numero);
   });
-});
+};
+pixelsAdd();
 
+const delDivsPixel = () => {
+  const deletPixel = document.querySelectorAll('.pixel');
+  for (let i = 0; i < deletPixel.length; i += 1) {
+    deletPixel[i].remove();
+  }
+};
